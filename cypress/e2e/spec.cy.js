1e6cf62f51908f965a714f756a67629d28238455
Kou <hikkappi@yahoo.co.jp>
2023-02-14 21:55:53 +0900

    引き分けの場合のE2Eテストを実装。

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -8,4 +8,18 @@ describe("My First Test", () => {
     cy.get("[data-e2e=8]").click();
     cy.get("[data-e2e=status]").should("have.text", "Winner: X");
   });
+  it("Draw Test", () => {
+    cy.visit("http://localhost:3001/");
+    cy.get("[data-e2e=0]").click();
+    cy.get("[data-e2e=1]").click();
+    cy.get("[data-e2e=4]").click();
+    cy.get("[data-e2e=8]").click();
+    cy.get("[data-e2e=6]").click();
+    cy.get("[data-e2e=2]").click();
+    cy.get("[data-e2e=5]").click();
+    cy.get("[data-e2e=3]").click();
+    cy.get("[data-e2e=7]").click();
+    cy.get("[data-e2e=status]").should("have.text", "Draw!");
+    cy.get("[data-e2e=status]").should("not.contain", "Winner");
+  });
 });
