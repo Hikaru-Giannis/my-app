21b6f226ac1bb8664327520bd6935a26f5a7de54
Kou <hikkappi@yahoo.co.jp>
2023-02-15 23:35:32 +0900

    baseUrlにURLを設定。

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,6 +1,6 @@
 describe("My First Test", () => {
   it("X player is Winnter Test", () => {
-    cy.visit("http://localhost:3001/");
+    cy.visit("/");
     cy.get("[data-e2e=0]").click();
     cy.get("[data-e2e=1]").click();
     cy.get("[data-e2e=4]").click();
@@ -9,7 +9,7 @@ describe("My First Test", () => {
     cy.get("[data-e2e=status]").should("have.text", "Winner: X");
   });
   it("Draw Test", () => {
-    cy.visit("http://localhost:3001/");
+    cy.visit("/");
     cy.get("[data-e2e=0]").click();
     cy.get("[data-e2e=1]").click();
     cy.get("[data-e2e=4]").click();
